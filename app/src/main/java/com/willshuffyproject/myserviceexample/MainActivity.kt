bbfe415223b9ead47fe67ba96be0fd623f90530b
willshuffy <saputrawilly26@gmail.com>
2020-10-27 13:18:56 +0700

    Create Layout

--- app/src/main/java/com/willshuffyproject/myserviceexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myserviceexample/MainActivity.kt
@@ -2,11 +2,40 @@ package com.willshuffyproject.myserviceexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start_service.setOnClickListener(this)
+        btn_start_intent_service.setOnClickListener(this)
+        btn_start_bound_service.setOnClickListener(this)
+        btn_stop_bound_service.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v:View){
+        when(v.id){
+
+            R.id.btn_start_service ->{
+
+            }
+
+            R.id.btn_start_intent_service ->{
+
+            }
+
+            R.id.btn_start_bound_service ->{
+
+            }
+
+            R.id.btn_stop_bound_service ->{
+
+            }
+        }
     }
 }
